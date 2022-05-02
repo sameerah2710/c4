fa8ce8d16c58e140d6ef62b507a52015927011bf
Sameer Ajay Harankhedkar <95958744+sameerah2710@users.noreply.github.com>
2022-05-02 16:56:56 +0530

    final

--- my-app/src/Redux/reducer.js
+++ my-app/src/Redux/reducer.js
@@ -1,3 +1,5 @@
+import { LOGOUT } from "./actions";
+
 const init = {
   username:"",
   role:"",
@@ -6,6 +8,18 @@ const init = {
 
 export const reducer = (store = init, { type, payload }) => {
   switch (type) {
+    case LOGIN:
+      return{
+        ...store,
+        username:payload.username,
+        role:payload.role,
+        isLoggedin: true,
+      };
+      case LOGOUT:
+        return{
+          ...store,
+          isLoggedin:false,
+        };
     default:
       return store;
   }
